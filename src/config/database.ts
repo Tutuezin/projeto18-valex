f454fca3ab0289a461392fd4e5036f5e93426098
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-08-30 18:42:30 -0300

    :tada: first commit!

---
+++ src/config/database.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import pg from "pg";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const connection = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default connection;
